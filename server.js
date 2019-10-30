f6801dfbfae96e1a01f4ebae57ac2f022bfa4fee
Shivam Vashney <A11IG1IL@airtelworld.com>
2019-10-30 12:25:42 +0530

    added git ignore file

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
 console.log('Shivam Varshney');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.json());
+console.log('Shivam Varshney After Updating');
